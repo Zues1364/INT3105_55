d6e7bc907a27571ae654e365525cd1728e53bfd3
Nguyen Tuan Duong <tuanduong152004@gmail.com>
2024-12-02 01:41:57 +0700

    Fix gatewayConfig

--- api-gateway/config/gatewayConfig.js
+++ api-gateway/config/gatewayConfig.js
@@ -1,15 +1,21 @@
+import rateLimit from 'express-rate-limit';
+
+const shortenerRateLimiting = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes in milliseconds
+    max: 100, // Maximum number of requests per window
+    message: 'Too many requests, please try again later.', // Message for rate limit exceed
+});
+
+const retrievalRateLimiting = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes in milliseconds
+    max: 100, // Maximum number of requests per window
+    message: 'Too many requests, please try again later.', // Message for rate limit exceed
+});
+
 const config = {
     port: process.env.PORT || 3000, // Port for the API Gateway
-    retrievalRateLimiting: {
-        windowMs: 15 * 60 * 1000, // 15 minutes in milliseconds
-        maxRequests: 100, // Maximum number of requests per window
-        message: 'Too many requests, please try again later.', // Message for rate limit exceed
-    },
-    shortenerRateLimiting: {
-        windowMs: 15 * 60 * 1000, // 15 minutes in milliseconds
-        maxRequests: 100, // Maximum number of requests per window
-        message: 'Too many requests, please try again later.', // Message for rate limit exceed
-    },
+    retrievalRateLimiting, // Use the middleware function
+    shortenerRateLimiting, // Use the middleware function
     services: {
         urlShortener: process.env.URL_SHORTENER_SERVICE || 'http://localhost/shorten', // NGINX load balancer for Shortener Service
         urlRetrieval: process.env.URL_RETRIEVAL_SERVICE || 'http://localhost/retrieve', // NGINX load balancer for Retrieval Service
@@ -17,8 +23,8 @@ const config = {
     mongodb: {
         host: 'mongodb://localhost:27017',
         database: 'url_shortener',
-        collection: 'main'
-    } 
+        collection: 'main',
+    },
 };
 
 export default config;
